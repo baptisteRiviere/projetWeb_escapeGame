151ea39b21d5ff41abaad49f5fac9d1382574af4
unknown <riviereb85@gmail.com>
2022-04-09 22:49:28 +0200

    preparation pour mise en ligne

--- connect.php
+++ connect.php
@@ -1,6 +1,7 @@
 <?php
 
-  $link =  mysqli_connect('localhost','root','root','escape');
+  //$link =  mysqli_connect('localhost','root','root','escape');
+  $link = mysqli_connect('mysql-riviereb.alwaysdata.net', 'riviereb_code', 'mdpPeuSécurisé', 'riviereb_escape');
   $link->options(MYSQLI_OPT_INT_AND_FLOAT_NATIVE, 1);
   mysqli_set_charset($link, "utf8");
   if (mysqli_connect_error()) {
